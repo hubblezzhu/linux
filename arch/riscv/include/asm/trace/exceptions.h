f32a6bef79b061264e83b1f40be2e1827d833096
Zhu Hengbo <zhuhengbo@iscas.ac.cn>
2024-07-15 07:40:08 +0000

    WIP
    Signed-off-by: Zhu Hengbo <zhuhengbo@iscas.ac.cn>

--- arch/riscv/include/asm/trace/exceptions.h
+++ arch/riscv/include/asm/trace/exceptions.h
@@ -6,13 +6,13 @@
  *
  */
 
-#undef TRACE_SYSTEM
-#define TRACE_SYSTEM exceptions
-
 #if !defined(_TRACE_PAGE_FAULT_H) || defined(TRACE_HEADER_MULTI_READ)
 #define _TRACE_PAGE_FAULT_H
 
 #include <linux/tracepoint.h>
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM exceptions
 
 TRACE_EVENT(page_fault,
 	TP_PROTO(struct pt_regs *regs),
@@ -35,10 +35,9 @@ TRACE_EVENT(page_fault,
 #endif /* _TRACE_PAGE_FAULT_H */
 
 #undef TRACE_INCLUDE_PATH
-#undef TRACE_INCLUDE_FILE
 #define TRACE_INCLUDE_PATH .
-#define TRACE_INCLUDE_FILE exceptions
-#endif /*  _TRACE_PAGE_FAULT_H */
+#undef TRACE_INCLUDE_FILE
+#define TRACE_INCLUDE_FILE trace
 
 /* This part must be outside protection */
 #include <trace/define_trace.h>
